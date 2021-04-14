d0df95dd1590e5b1618dc7756540034c638b5e26
David Cruz Ramírez <davoh.dev@gmail.com>
2021-04-14 18:30:05 -0500

    inject component in mainActivity

--- app/src/main/java/com/davoh/oauth2_twitch/MainActivity.kt
+++ app/src/main/java/com/davoh/oauth2_twitch/MainActivity.kt
@@ -2,10 +2,12 @@ package com.davoh.oauth2_twitch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.davoh.oauth2_twitch.di.MainApplication
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        (applicationContext as MainApplication).getComponent().inject(this)
     }
 }
