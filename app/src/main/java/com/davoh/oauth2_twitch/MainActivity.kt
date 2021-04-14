6d2df99ba3102bcce84182f021d15a75e4a72571
David Cruz Ramírez <davoh.dev@gmail.com>
2021-04-14 18:34:41 -0500

    twitchOAuth2 injected in mainActivity

--- app/src/main/java/com/davoh/oauth2_twitch/MainActivity.kt
+++ app/src/main/java/com/davoh/oauth2_twitch/MainActivity.kt
@@ -4,9 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.davoh.oauth2_twitch.databinding.ActivityMainBinding
 import com.davoh.oauth2_twitch.di.MainApplication
+import com.davoh.oauth2_twitch.framework.TwitchOAuth2
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    @Inject
+    lateinit var twitchOAuth2: TwitchOAuth2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
