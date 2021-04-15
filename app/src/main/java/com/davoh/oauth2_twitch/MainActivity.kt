ca683ee3b68942807443893a201f2bca3e36a158
David Cruz Ramírez <davoh.dev@gmail.com>
2021-04-15 13:59:27 -0500

    CustomWebTabs implemented

--- app/src/main/java/com/davoh/oauth2_twitch/MainActivity.kt
+++ app/src/main/java/com/davoh/oauth2_twitch/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.davoh.oauth2_twitch
 
-import android.R
+import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
+import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
-import android.view.View
-import android.webkit.WebSettings
-import android.webkit.WebView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.browser.customtabs.CustomTabColorSchemeParams
+import androidx.browser.customtabs.CustomTabsClient
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.browser.customtabs.CustomTabsIntent.COLOR_SCHEME_SYSTEM
+import androidx.browser.customtabs.CustomTabsServiceConnection
 import com.davoh.oauth2_twitch.databinding.ActivityMainBinding
 import com.davoh.oauth2_twitch.di.MainApplication
 import com.davoh.oauth2_twitch.framework.AccessToken
@@ -33,16 +39,30 @@ class MainActivity : AppCompatActivity() {
 
         binding.btnLogin.setOnClickListener {
             val responseType = "code"
-            val redirectUri= "http://localhost"
-            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://id.twitch.tv/oauth2/authorize?response_type=${responseType}&client_id=1tdrqc3epx025bimv640owygxn5vaq&redirect_uri=${redirectUri}&scope=viewing_activity_read&state=c3ab8aa609ea11e793ae92361f002671"))
-            startActivity(intent)
+            val redirectUri = "http://localhost"
+
+            val url = "https://id.twitch.tv/oauth2/authorize?response_type=${responseType}&client_id=1tdrqc3epx025bimv640owygxn5vaq&redirect_uri=${redirectUri}&scope=viewing_activity_read&state=c3ab8aa609ea11e793ae92361f002671"
+            launchCustomWebTab(Uri.parse(url))
         }
+    }
+
+    private fun launchCustomWebTab(uri:Uri){
+        val builder = CustomTabsIntent.Builder()
+        val colorInt = Color.parseColor("#FF6200EE")
+
+        val builderCustomTabColorSchemeParams = CustomTabColorSchemeParams.Builder()
+        builderCustomTabColorSchemeParams.setToolbarColor(colorInt)
+
+        builder.setDefaultColorSchemeParams(builderCustomTabColorSchemeParams.build())
+        val customTabsIntent: CustomTabsIntent = builder.build()
+        customTabsIntent.intent.flags = FLAG_ACTIVITY_NEW_TASK
+        customTabsIntent.launchUrl(this, uri)
     }
 
     override fun onResume() {
         super.onResume()
         val uri = intent.data
-        if (uri!=null && uri.toString().startsWith("http://localhost")){
+        if (uri != null && uri.toString().startsWith("http://localhost")) {
             val code = uri.getQueryParameter("code")
             val call = twitchOAuth2.getToken("1tdrqc3epx025bimv640owygxn5vaq",
                     "p0xsoya6rt9dfiw1p4odgw1l9cujfi",
